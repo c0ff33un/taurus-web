df512edf29b31a99f136b76da96146a8682d04e4
Edwin M <ermahechap@unal.edu.co>
2019-10-26 15:53:52 -0500

    Redux ready

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import Routes from './Routes';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { render } from 'react-dom';
+
+
+import rootReducer from '@r/reducers'
+
+const store = createStore(rootReducer);
+
 render(
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>,
+    <Provider store ={store}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Provider>,
     document.querySelector('#root')
   )
 // If you want your app to work offline and load faster, you can change
